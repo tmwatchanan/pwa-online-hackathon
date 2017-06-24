dc9b62cb136dcee1db18a4dcb9433f1fffd3f42b
tmwatchanan <tmwatchanan@gmail.com>
2017-06-24 23:31:06 +0700

    Adjust config

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "public\\",
+  "globPatterns": [
+    "**/*.html"
+  ],
+  "swDest": "public/sw.js",
+  "globIgnores": [
+    "..\\workbox-cli-config.js"
+  ]
+};
