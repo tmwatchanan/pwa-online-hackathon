ef4156220f5d0445848ab0d6dd9e6c20c7f2b28d
tmwatchanan <tmwatchanan@gmail.com>
2017-06-25 17:13:28 +0700

    Change from NodeJS to jQuery POST request

--- public/js/notification.js
+++ public/js/notification.js
@@ -19,33 +19,6 @@ function SubscribeToRoom(roomName, userId) {
     }]);
 }
 
-var sendNotification = function(data) {
-  var headers = {
-    "Content-Type": "application/json; charset=utf-8",
-    "Authorization": "Basic NGEwMGZmMjItY2NkNy0xMWUzLTk5ZDUtMDAwYzI5NDBlNjJj"
-  };
-  
-  var options = {
-    host: "onesignal.com",
-    port: 443,
-    path: "/api/v1/notifications",
-    method: "POST",
-    headers: headers
-  };
-  
-  var https = require('https');
-  var req = https.request(options, function(res) {  
-    res.on('data', function(data) {
-      console.log("Response:");
-      console.log(JSON.parse(data));
-    });
-  });
-  
-  req.on('error', function(e) {
-    console.log("ERROR:");
-    console.log(e);
-  });
-  
-  req.write(JSON.stringify(data));
-  req.end();
-};
+function sendNotification(messageJson) {
+  $.post("https://onesignal.com/api/v1/notifications", messageJson)
+}
