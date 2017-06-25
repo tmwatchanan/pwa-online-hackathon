ae8a89e947057b8e0f4c906758e82e3ac8451580
tmwatchanan <tmwatchanan@gmail.com>
2017-06-25 14:53:58 +0700

    Prepare subscription

--- public/js/notification.js
+++ public/js/notification.js
@@ -1,9 +1,8 @@
 function SubscribeToRoom(roomName) {
     var welcomeMsg = 'คุณได้ยืนยันรับข้อความแจ้งเตือนจาก ' + roomName + '\nขอบคุณมากครับ';
     
-
     var OneSignal = window.OneSignal || [];
-    OneSignal.push(["sendTag", "room_name", ""])
+    OneSignal.push(["sendTag", "room_name", roomName])
     OneSignal.push(["init", {
         appId: "00166562-147d-4eaa-95ae-49788c7c9744",
         autoRegister: false,
