8f22af213359e5a3b5f7cd7b43147913fca717af
Chawasit Tengtrairatana <chawasit_teng@cmu.ac.th>
2017-06-25 15:11:37 +0700

    added create room & message

--- public/js/application.js
+++ public/js/application.js
@@ -1,2 +1,22 @@
+function createRoom(roomName, ownerId) {
+  firebase.database().ref('rooms/' + roomName).set({
+    ownerId: ownerId,
+  });
+}
+
+function createMessage(roomName, message) {
+  var postData = {
+    message: message,
+    createdAt: new Date().toLocaleString()
+  };
+
+  var newPostKey = firebase.database().ref().child('rooms/' + roomName).push().key;
+
+  var updates = {};
+  updates['/rooms/' + roomName + '/' + newPostKey] = postData;
+
+  return firebase.database().ref().update(updates);
+}
+
 
 $.material.init()
